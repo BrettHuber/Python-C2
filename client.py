c65a1004829d294c5ddac7c489a56f0c7e66f02b
Brett Huber <hawktalon505@gmail.com>
2023-04-11 22:40:06 -0400

    Added try and except statements

--- client.py
+++ client.py
@@ -3,3 +3,10 @@ import subprocess
 
 hostAddress = "localhost"
 portNumber = 6969
+
+try:
+    x = ""
+except:
+    x = ""
+finally:
+    x = ""
--- server.py
+++ server.py
@@ -3,3 +3,9 @@ import socket
 hostAddress = "localhost" # IP Address of machine that runs server
 portNumber = 6969
 
+try:
+    x = ""
+except:
+    x = ""
+finally:
+    x = ""
