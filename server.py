72e6e01e6e623b16df471a7c3e7636c44cc00e47
Brett Huber <hawktalon505@gmail.com>
2023-04-11 23:10:25 -0400

    Added while loop and command transmission/input to server.py

--- server.py
+++ server.py
@@ -8,12 +8,18 @@ sock.bind((hostAddress, portNumber)) # Binds a socket to a specific host address
 sock.listen(5) # Specifies that up to five connections can be handled by the socket via a queue
 
 print(f"Listening on {hostAddress} at port {portNumber}:") # Prints out the host address and port number
-connection, address = sock.accept() # Accepts a client socket connection and gets the client socket connection and address 
+clientConnection, address = sock.accept() # Accepts a client socket connection and gets the client socket connection and address 
 print(f"Connected to by {address}") # Prints out the client address
 
 try:
-    x = ""
+    while True: # Runs a loop until break statement is triggered
+        inputCommand = input(">> ") # Takes the command as input
+        if not inputCommand.strip() or inputCommand.upper() == "QUIT": # Checks if input was quit or nothing
+            break # Breaks the loop
+        clientConnection.sendall(inputCommand.encode()) # Sends the client connection the encoded command
+        commandOutput = clientConnection.recv(4096).decode() # Gets the output from the client
+        print(commandOutput) # Prints output of command when executed on client
 except Exception as error:
-    print(f"Error Message: {error}")
+    print(f"Error Message: {error}") # Prints error message
 finally:
     x = ""
