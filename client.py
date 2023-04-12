b59c17b487e7b19c447de11f09e9e8eaa7298cdb
Brett Huber <hawktalon505@gmail.com>
2023-04-11 23:15:44 -0400

    Added client socket creation and connection, socket closed to client.py

--- client.py
+++ client.py
@@ -4,10 +4,11 @@ import subprocess # Imports subprocess module
 hostAddress = "localhost" # IP Address of machine that runs server
 portNumber = 6969 # Specifies the port used for communication
 
-
 try:
-    x = ""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Creates a TCP and IPV4 socket
+    sock.connect((hostAddress, portNumber)) # Connect to the server socket
+
 except Exception as error:
-    print(f"Error Message: {error}")
+    print(f"Error Message: {error}") # Prints error message
 finally:
-    x = ""
+    sock.close() # Closes the client socket
