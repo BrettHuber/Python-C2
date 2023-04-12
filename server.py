ca35c38fa9920841a01c8ca7f8d27ebdd8ca0e6e
Brett Huber <hawktalon505@gmail.com>
2023-04-11 22:50:08 -0400

    Added socket creation, information print messages, accepts client socket connection to server.py

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
-import socket
+import socket # Imports socket module
 
 hostAddress = "localhost" # IP Address of machine that runs server
-portNumber = 6969
+portNumber = 6969 # Specifies the port used for communication
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Creates a TCP and IPV4 socket
+sock.bind((hostAddress, portNumber)) # Binds a socket to a specific host address and port number
+sock.listen(5) # Specifies that up to five connections can be handled by the socket via a queue
+
+print(f"Listening on {hostAddress} at port {portNumber}:") # Prints out the host address and port number
+connection, address = sock.accept() # Accepts a client socket connection and gets the client socket connection and address 
+print(f"Connected to by {address}") # Prints out the client address
 
 try:
     x = ""
